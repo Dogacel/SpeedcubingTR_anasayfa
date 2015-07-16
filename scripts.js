d9576d4a2df31c56ff321237492d93befac090ec
Dogacel <dogacel@gmail.com>
2015-07-17 01:41:29 +0300

    index2
    index2 w/can

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-jQuery(
-    function($){
-    $(".tap").children("li").children("a").click(function(){
-        $(".tap").children(".active").removeClass("active");
-        $(this).parent().addClass("active");
-        if($(this).hasClass("clc"))
-        {
-            $(".cozumler").slideDown("slow");
-        }else{
-            $(".cozumler").slideUp("slow");
-        }
-    });
-});
-
-
+function cozumdown(){
+    $(".navbar-cozumler").slideDown();
+}
