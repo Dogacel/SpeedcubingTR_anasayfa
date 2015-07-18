6d95142c64155a8e0a317b324a144fe8795c9ad5
Dogacel <dogacel@gmail.com>
2015-07-18 18:07:04 +0300

    First release 1.0
    finished first release 1.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
 function displaynone(){
-    $(".content").children("p").slideUp('slow');
+    $("div.content").children("div").slideUp('slow');
 }
 function anasayfa(){  
-    $(".content").children("p.anasayfa").slideDown('slow',displaynone());}
+    $("div.content").children("div.anasayfa").slideDown('slow',displaynone());}
 function cevirici(){
     displaynone();
-    $(".content").children(".ceviriciler").slideDown('slow',displaynone());}
+    $("div.content").children("div.ceviriciler").slideDown('slow',displaynone());}
 function comez(){
     displaynone();
-    $(".content").children("p.comez").slideDown('slow',displaynone());}
+    $("div.content").children("div.comez").slideDown('slow',displaynone());}
 function boll(){
     displaynone();
-    $(".content").children("p.basitoll").slideDown('slow',displaynone());}
+    $("div.content").children("div.basitoll").slideDown('slow',displaynone());}
 function keyhole(){
     displaynone();
-    $(".content").children("p.keyhole").slideDown('slow',displaynone());}
+    $("div.content").children("div.keyhole").slideDown('slow',displaynone());}
 function a444(){
     displaynone();
-    $(".content").children("p.444").slideDown('slow',displaynone());}
+    $("div.content").children("div.444").slideDown('slow',displaynone());}
 function bld(){
     displaynone();
-    $(".content").children("p.bld").slideDown('slow',displaynone());}
+    $("div.content").children("div.bld").slideDown('slow',displaynone());}
 function a222(){
     displaynone();
-    $(".content").children("p.222").slideDown('slow',displaynone());}
+    $("div.content").children("div.222").slideDown('slow',displaynone());}
 function f2l(){
     displaynone();
-    $(".content").children("p.f2l").slideDown('slow',displaynone());}
+    $("div.content").children("div.f2l").slideDown('slow',displaynone());}
 function oll(){
     displaynone();
-    $(".content").children("p.oll").slideDown('slow',displaynone());}
+    $("div.content").children("div.oll").slideDown('slow',displaynone());}
 function pll(){
     displaynone();
-    $(".content").children("p.pll").slideDown('slow',displaynone());}
+    $("div.content").children("div.pll").slideDown('slow',displaynone());}
 function forum(){
     $("html").fadeOut();                  
     window.location.replace("http://www.speedcubingtr.com/forum/");
